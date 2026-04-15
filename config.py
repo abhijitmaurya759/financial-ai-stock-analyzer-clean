ab86c77192ec26f26fa4905c6089f34320d69132
abhijitmaurya759 <abhijitmaurya759@gmail.com>
2026-04-15 23:28:47 +0530

    Fix: add streamlit secrets support

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or st.secrets.get("OPENAI_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY") or st.secrets.get("NEWS_API_KEY")
